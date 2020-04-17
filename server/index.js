1ecfa216e2e5f398a02643bac85592c408b8195e
jon-zawada <jonathan_zawada@yahoo.com>
2020-04-16 23:37:26 -0700

    adds working proxy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const httpProxy = require("http-proxy");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = 9000;
 
 const proxy = httpProxy.createProxyServer({});
@@ -12,54 +13,12 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.static(path.join(__dirname, '../client')));
 
-app.all('/api/mainSong', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-18-144-8-70.us-west-1.compute.amazonaws.com:3003"
-  });
-});
+app.use('/api/songs/', createProxyMiddleware({ target: 'http://54.241.202.220'}));
+app.use('/api/comments/songId', createProxyMiddleware({ target: 'http://54.241.202.220'}));
+app.use('/songs', createProxyMiddleware({ target: 'http://3.21.230.11:3000'}));
+app.use('/api/songId', createProxyMiddleware({ target: 'http://54.151.19.84:4001'}));
 
-// app.get('/api/mainSong', (req, res) => {
-//   res.redirect("http://ec2-18-144-8-70.us-west-1.compute.amazonaws.com:3003/api/mainSong")
-// })
 
-app.all('/songData', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-54-219-75-95.us-west-1.compute.amazonaws.com:3001/"
-  });
-});
-
-// app.get('/songData', (req, res) => {
-//   res.redirect("http://ec2-18-144-8-70.us-west-1.compute.amazonaws.com:3003/songData")
-// })
-
-app.all('/songs', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-52-53-195-134.us-west-1.compute.amazonaws.com/"
-  })
-});
-
-// app.get('/sonogs', (req, res) => {
-//   res.redirect("http://ec2-18-144-8-70.us-west-1.compute.amazonaws.com:3003/songs")
-// })
-
-app.all('/api/comments', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-18-144-133-175.us-west-1.compute.amazonaws.com:4001/"
-  })
-});
-
-app.all('/api/reply', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-18-144-133-175.us-west-1.compute.amazonaws.com:4001/"
-  })
-});
-
-app.all('/api/tracker', (req, res) => {
-  proxy.web(req, res, {
-    target: "http://ec2-18-144-133-175.us-west-1.compute.amazonaws.com:4001/"
-  })
-});
 
 app.listen(port, () => console.log(`CAN YOU HEAR ME @ ${port}`));
